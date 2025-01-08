254d1101a45ff0aa4336f6e3590de26814ee3045
mohitcsb <mohitcsb@gmail.com>
2025-01-08 12:32:08 +0600

    Stack
    Stack and blur images

--- mypro5/lib/login.dart
+++ mypro5/lib/login.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -11,20 +13,98 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-        body: Column(
+      body: Column(
         children: [
-          Text(
-              "Login",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 24.0, // Adjust the text size
-            fontWeight: FontWeight.bold, // Make the text bold (optional)
-            color: Colors.blue,
-          )
-          )
-          
-        ]
+          Container(
+            height: 400,
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              children: [
+                ImageFiltered(
+                  imageFilter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                  child: Image.asset("images/sale_banner.jpg",width: double.infinity,),
+                ),
+                Positioned(
+                  left: 10,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.lightBlueAccent,
+                    ),
+                    width: 60,
+                    height: 60,
+                  ),
+                ),
+                Positioned(
+                  left: 30,
+                  child: Container(
+                    width: 60.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      color: const Color(0xff7c94b6),
+                      image: DecorationImage(
+                        image: AssetImage('images/product2.png'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                      border: Border.all(
+                        color: Colors.red,
+                        width: 8.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 100,
+                  right: 70,
+                  child: Container(
+                    width: 100.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: const Color(0xff7c94b6),
+                      image: DecorationImage(
+                        image: AssetImage('images/product3.png'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                      border: Border.all(
+                        color: Colors.blueAccent,
+                        width: 8.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 100,
+                  right: 100,
+                  child: Container(
+                    width: 100.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      color: const Color(0xff7c94b6),
+                      image: DecorationImage(
+                        image: AssetImage('images/product4.png'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                      border: Border.all(
+                        color: Colors.blueAccent,
+                        width: 8.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Text("Login",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24.0, // Adjust the text size
+                fontWeight: FontWeight.bold, // Make the text bold (optional)
+                color: Colors.blue,
+              ))
+        ],
       ),
     );
   }
